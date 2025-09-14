207c96dbdee6a6a64affc819f21af99e5c34ab8b
aviraj5421 <avirajschilgunde@gmail.com>
2025-09-14 15:32:55 +0530

    initial commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,57 @@
+import React, { useContext, useEffect, useState } from 'react'
+import './Home.css'
+import { CoinContext } from '../../context/CoinContext'
+
+function Home() {
+  const {allCoin,currency}=useContext(CoinContext);
+
+  const [displayCoin,setDisplayCoin]=useState([]);
+
+  useEffect(()=>{
+    setDisplayCoin(allCoin);
+
+  },[allCoin])
+
+  return (
+    <div className='home'>
+      <div className="hero">
+        <h1>Largest <br/> Crypto Marketplace</h1>
+        <p>welcome to the world's largest cryptocurrency marketplace .
+           Sign up to explore more about cryptos</p>
+     <form action="">
+
+      <input type="text" placeholder='search crypto...' />
+      <button type="submit">Search</button>
+     </form>
+      
+      </div>
+
+      <div className="crypto-table">
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Price</p>
+          <p style={{textAlign:"center"}}>24H Change</p>
+          <p className='market-cap'>Market Cap</p>
+        </div>
+    
+   { displayCoin.slice(0,10).map((item,index) => (
+      <div className="table-layout" key={index}>
+        <p>{item.market_cap_rank}</p>
+
+       
+       
+      </div>
+   
+    ))
+   }
+        
+          
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Home
